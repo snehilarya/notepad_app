e9798d1b826e278959168b777248b06a94ba6cb8
Snehil Arya <snehil.arya@sap.com>
2021-12-18 21:57:23 +0530

    adding name as parameter

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -4,11 +4,13 @@ import 'user_preference.dart';
 
 class User {
   String userId;
+  String displayName;
   String emailId;
   UserPreferences userPreferences;
 
   User({
     @required this.userId,
+    @required this.displayName,
     @required this.emailId,
     this.userPreferences = null,
   });
--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -3,7 +3,7 @@ import '../entities/user.dart';
 abstract class AuthRepository {
   Future<User> login(String email, String password);
 
-  Future<User> signup(String email, String password);
+  Future<User> signup(String name, String email, String password);
 
   Future<bool> checkEmailVerified();
 
