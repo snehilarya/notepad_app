f880406f6fee2f46d955304ab05bcd4258902b5e
Snehil Arya <snehil.arya@sap.com>
2021-12-18 21:50:30 +0530

    add user entity

--- lib/features/auth/domain/entities/user.dart
+++ lib/features/auth/domain/entities/user.dart
@@ -4,13 +4,11 @@ import 'user_preference.dart';
 
 class User {
   String userId;
-  String displayName;
   String emailId;
   UserPreferences userPreferences;
 
   User({
     @required this.userId,
-    @required this.displayName,
     @required this.emailId,
     this.userPreferences = null,
   });
